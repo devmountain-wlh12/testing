a024c32590811c83ba3545441c0b67be15a1126a
Jonathan McDonald <jonathanlmcd@gmail.com>
2019-11-11 23:21:49 -0700

    lecture notes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    input: '',
+    todos: ['Practice the Piano']
+  }
+
+  addTodo() {
+    this.setState({
+      todos: [...this.state.todos, this.state.input]
+    })
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        {this.state.todos.map(el => (
+          <li key={el}>{el}</li>
+        ))}
+        <input
+          onChange={e => this.setState({ input: e.target.value })}
+          type="text"
+          value={this.state.input}
+        />
+        <button onClick={() => this.addTodo()}>Add Todo</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
